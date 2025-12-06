f28b2cbc223b70e629784400d624c5d43c35feca
ismailatibatwit <144959357+ismailatibatwit@users.noreply.github.com>
2025-12-06 17:13:54 +0000

    Refactor parta.c for file reading and character counting

--- parta.c
+++ parta.c
@@ -1,45 +1,52 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
-#include <unistd.h>
+#include <ctype.h>
 #include <fcntl.h>
-#include <sys/wait.h>
+#include <unistd.h>
+
+///reads an file in 8-bytes and conts char categories in (upper,lower,digits,space and other)
 
 
-///sends stdout to output file
-int main(int argc, char* argv[]) {
-    if (argc != 3) {
-        printf("USAGE: partb FILEIN FILEOUT\n");
+int main(int argc, char *argv[]) {
+    enum { BUFSIZE = 8 };
+    char buffer[BUFSIZE];
+
+    if (argc != 2) {
+        printf("USAGE: parta FILEIN");
         return 1;
     }
 
-    const char *filein = argv[1];
-    const char *fileout = argv[2];
+    const char *filename = argv[1];
 
-    int fd = open(fileout, O_WRONLY | O_CREAT | O_TRUNC, 0770);
+    int fd = open(filename, O_RDONLY);
     if (fd < 0) {
-        perror("open");
-        return 1;
+        printf("ERROR: %s not found", filename);
+        return 2;
     }
-    if (dup2(fd, STDOUT_FILENO) < 0) {
-        perror("dup2");
-        close(fd);
-        return 1;
+
+    int upper = 0, lower = 0, number = 0, space = 0, other = 0;
+    ssize_t nread;
+
+    while ((nread = read(fd, buffer, BUFSIZE)) > 0) {
+        for (ssize_t i = 0; i < nread; i++) {
+            unsigned char c = (unsigned char)buffer[i];
+            if (isupper(c))       upper++;
+            else if (islower(c))  lower++;
+            else if (isdigit(c))  number++;
+            else if (isspace(c))  space++;
+            else                  other++;
+        }
     }
+
     close(fd);
 
-    printf("Category,Count\n");
-    fflush(stdout);
+    printf("Upper,%d\n", upper);
+    printf("Lower,%d\n", lower);
+    printf("Number,%d\n", number);
+    printf("Space,%d\n", space);
+    printf("Other,%d\n", other);
 
+    return 0;
 
-    char *args[] = {
-        "./parta",
-        (char *)filein,
-        NULL
-    };
-
-    execv("./parta", args);
-
-    perror("execv");
-    return 1;
 }
