3982ccce854370ae2317febfc71ba83bfa5f6e74
ismailatibatwit <144959357+ismailatibatwit@users.noreply.github.com>
2025-12-06 17:12:46 +0000

    Implement file handling and output redirection
    Added file input and output handling in main function.

--- parta.c
+++ parta.c
@@ -1,15 +1,45 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
-#include <ctype.h>
-#include <fcntl.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <sys/wait.h>
 
+
+///sends stdout to output file
 int main(int argc, char* argv[]) {
-    // Declare the buffer to use. Do NOT change!
-    enum { BUFSIZE = 8 };
-    char buffer[BUFSIZE];
-    // TODO: Complete and document
+    if (argc != 3) {
+        printf("USAGE: partb FILEIN FILEOUT\n");
+        return 1;
+    }
 
-    return 0;
+    const char *filein = argv[1];
+    const char *fileout = argv[2];
+
+    int fd = open(fileout, O_WRONLY | O_CREAT | O_TRUNC, 0770);
+    if (fd < 0) {
+        perror("open");
+        return 1;
+    }
+    if (dup2(fd, STDOUT_FILENO) < 0) {
+        perror("dup2");
+        close(fd);
+        return 1;
+    }
+    close(fd);
+
+    printf("Category,Count\n");
+    fflush(stdout);
+
+
+    char *args[] = {
+        "./parta",
+        (char *)filein,
+        NULL
+    };
+
+    execv("./parta", args);
+
+    perror("execv");
+    return 1;
 }
